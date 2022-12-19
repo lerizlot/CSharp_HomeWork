9bca4d4e41b7e681154d95aa3db7b397ed6070d9
Lera <lerizlot@icloud.com>
2022-12-19 21:48:32 +0400

    task 3 of 3 has been completed

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -3,44 +3,60 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-
-int SecondNumber(int number)
+/*
+int SecondNumber(int num)
 {
-    int count = number % 100 / 10;
+    int count = num % 100 / 10;
     return count;
 }
-Console.Write("Input your number: ");
+Console.Write("Task1. Input your number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int secondNumber = SecondNumber(num);
 
-// необязательная по условию задачи проверка на 3-х значность
+// необязательная, по условию задачи, проверка на 3-х значность
 if (num <= 999 && num >= 100)
     Console.WriteLine($"The second number is {secondNumber}");
 else
     Console.WriteLine("Your number is wrong. Please input a three-digit number.");
-
+*/
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Task2. Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+    Console.WriteLine("There is no third digit");
+if (number > 99 && number < 1000)
+    Console.WriteLine($"The third digit is {number % 10}");
+if (number > 999 && number < 10000)
+    Console.WriteLine($"The third digit is {number / 10 % 10}");
+if (number > 9999 && number < 100000)
+    Console.WriteLine($"The third digit is {number / 100 % 10}");
+if (number > 99999 && number < 1000000)
+    Console.WriteLine($"The third digit is {number / 1000 % 10}");    
+
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 // 6 -> да
 // 7 -> да
 // 1 -> нет
-
+/*
 bool DayOfTheWeek (int numb)
 {
     if (numb == 6 || numb == 7) return true;
     else return false;
+    
 }
 
-Console.Write("Input your number: ");
+Console.Write("Task3. Input your number: ");
 int numb = Convert.ToInt32(Console.ReadLine());
 
 bool result = DayOfTheWeek(numb);
 
 Console.WriteLine($"The number you entered is a weekend: {result}");
+*/
