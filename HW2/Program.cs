5c70687ebc32396616780d436beb2a682d78e36c
Lera <lerizlot@icloud.com>
2022-12-19 16:56:18 +0400

    task 2 of 3 has been completed

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -6,21 +6,20 @@
 
 int SecondNumber(int number)
 {
-    int actionOne = number % 100; // 56
-    int actionTwo = actionOne / 10; // 5
-
-    return actionTwo;
+    int count = number % 100 / 10;
+    return count;
 }
 Console.Write("Input your number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int result = SecondNumber(num);
+int secondNumber = SecondNumber(num);
 
+// необязательная по условию задачи проверка на 3-х значность
 if (num <= 999 && num >= 100)
-    Console.WriteLine($"The second number is {result}");
+    Console.WriteLine($"The second number is {secondNumber}");
 else
     Console.WriteLine("Your number is wrong. Please input a three-digit number.");
-    
+
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 // 645 -> 5
@@ -32,3 +31,16 @@ else
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+bool DayOfTheWeek (int numb)
+{
+    if (numb == 6 || numb == 7) return true;
+    else return false;
+}
+
+Console.Write("Input your number: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+bool result = DayOfTheWeek(numb);
+
+Console.WriteLine($"The number you entered is a weekend: {result}");
