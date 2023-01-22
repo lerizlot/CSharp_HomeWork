661c66851547ef8f7ff2459618ee6ae1c1967269
Lera <lerizlot@icloud.com>
2023-01-22 21:14:44 +0400

    task 1 of 3 has been completed

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -2,6 +2,53 @@
 
 // [345, 897, 568, 234] -> 2
 
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+int CheckIfEven(int[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i] % 2 == 0)
+        {
+        count += 1;
+        }
+    }
+    return count;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("Task1: ");
+
+Console.Write("Input a number of elements: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value more than 100: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value less than 1000: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(length, min, max);
+ShowArray(newArray);
+
+int answer = CheckIfEven(newArray);
+Console.WriteLine($"The quantity of even numbers in your array is {answer}");
+Console.WriteLine("-----------------");
+
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 // [3, 7, 23, 12] -> 19
@@ -10,3 +57,13 @@
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
+
+// double[] CreateDoubleArray(int size, int minValue, int maxValue)
+// {
+//     double[] array = new double[size];
+
+//     for (int i = 0; i < size; i++)
+//         array[i] = new Random().NextDouble(minValue, maxValue + 1);
+
+//     return array;
+// }
