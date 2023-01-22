6f0e41b9c0d0a601fc5db7d758791f09332e9ca1
Lera <lerizlot@icloud.com>
2023-01-22 23:18:53 +0400

    all tasks have been completed

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -5,48 +5,37 @@
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     int[] array = new int[size];
-
     for (int i = 0; i < size; i++)
         array[i] = new Random().Next(minValue, maxValue + 1);
-
     return array;
-}
-
-int CheckIfEven(int[] newArray)
-{
-    int count = 0;
-    for (int i = 0; i < newArray.Length; i++)
-    {
-        if (newArray[i] % 2 == 0)
-        {
-        count += 1;
-        }
-    }
-    return count;
 }
 
 void ShowArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
-
     Console.WriteLine();
 }
 
-Console.WriteLine("Task1: ");
+int GetCountEven(int[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < newArray.Length; i++)
+        if (newArray[i] % 2 == 0) count++;
+    return count;
+}
+
+Console.WriteLine("Task 1: ");
 
 Console.Write("Input a number of elements: ");
-int length = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a min possible value more than 100: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a max possible value less than 1000: ");
-int max = Convert.ToInt32(Console.ReadLine());
+int length1 = Convert.ToInt32(Console.ReadLine());
+int min1 = 100;
+int max1 = 1000;
 
-int[] newArray = CreateRandomArray(length, min, max);
+int[] newArray = CreateRandomArray(length1, min1, max1);
 ShowArray(newArray);
 
-int answer = CheckIfEven(newArray);
-Console.WriteLine($"The quantity of even numbers in your array is {answer}");
+Console.WriteLine($"The quantity of even numbers in your array is {GetCountEven(newArray)}");
 Console.WriteLine("-----------------");
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
@@ -54,16 +43,70 @@ Console.WriteLine("-----------------");
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int GetSumOddPosition(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+        sum = sum + array[i];
+        Console.WriteLine($"{sum}");
+    return sum;
+}
+
+Console.WriteLine("Task 2: ");
+Console.Write("Input a number of elements: ");
+int length2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max2 = Convert.ToInt32(Console.ReadLine());
+
+int[] secondArray = CreateRandomArray(length2, min2, max2);
+ShowArray(secondArray);
+
+Console.WriteLine($"The sum of numbers located on the odd position in your array is {GetSumOddPosition(secondArray)}");
+Console.WriteLine("-----------------");
+
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
 
-// double[] CreateDoubleArray(int size, int minValue, int maxValue)
-// {
-//     double[] array = new double[size];
+double[] CreateRandomDoubleArray(int size, int minValue, int maxValue)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+        array[i] = Math.Round((new Random().Next(minValue, maxValue) + new Random().NextDouble()), 2);
+    return array;
+}
 
-//     for (int i = 0; i < size; i++)
-//         array[i] = new Random().NextDouble(minValue, maxValue + 1);
+void ShowDoubleArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.WriteLine(array[i]);
+    Console.WriteLine();
+}
 
-//     return array;
-// }
+double GetDifference(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for(int i = 1; i < array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+        if(array[i] > max) max = array[i];
+    }
+    double difference = Math.Round(max - min, 2);
+    Console.WriteLine($"{max} - {min} = {difference}");
+    return difference;
+}
+
+Console.WriteLine("Task 3: ");
+Console.Write("Input a number of elements: ");
+int length3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max3 = Convert.ToInt32(Console.ReadLine());
+
+double[] thirdArray = CreateRandomDoubleArray(length3, min3, max3);
+ShowDoubleArray(thirdArray);
+Console.WriteLine($"{GetDifference(thirdArray)}");
